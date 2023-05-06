f7ef6e241eddc4707eeca97d3c9f7f350632dcbd
João Vitor <joaovitorcfdev@gmail.com>
2023-05-06 14:37:49 -0300

    create project

---
+++ lib/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/bloc');
+              },
+              child: Text(
+                'Bloc',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/cubit');
+              },
+              child: Text(
+                'Cubit',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
